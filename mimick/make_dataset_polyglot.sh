26a5558a5a0e8d72a2894f34de6fc3da47039b81
Yoshinari Fujinuma <fujinumay@gmail.com>
2017-07-27 23:56:44 +0900

    Updating to relative dir

--- mimick/make_dataset_polyglot.sh
+++ mimick/make_dataset_polyglot.sh
@@ -1,5 +1,5 @@
 python3 make_dataset.py \
     --vectors polyglot-en.pkl \
-    --vocab /Users/yoshinarifujinuma/work/Mimick/vocabs/en-vocab.txt \
+    --vocab ../vocabs/en-vocab.txt \
     --output polyglot_en.out \
 #python make_dataset.py \
